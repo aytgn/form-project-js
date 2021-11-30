2e5c97927ec36985d757b3e34e7a2edfb3be0aba
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-30 13:09:34 +0300

    validation

--- add-user.form.js
+++ add-user.form.js
@@ -1,5 +1,6 @@
 //SELECT ELEMENTS
 //->form
+const employeeAddForm = document.querySelector("#employeeAddForm");
 const employeeNoEL = document.querySelector("#employeeNo");
 const firstNameEL = document.querySelector("#firstName");
 const employeeTitleEL = document.querySelector("#employeeTitle");
@@ -13,9 +14,9 @@ const employeePhoneEL = document.querySelector("#employeePhone");
 const bodyEL = document.querySelector("body");
 //->buttons
 const resetBtnEL = document.querySelector("#resetFormBtn");
-
-//EVENT LISTENERS
-resetBtnEL.addEventListener("click", (event) => {
+const submitBtnEl = document.querySelector("#submitEmployeeForm");
+//EVENT HANDLERS
+const resetBtnClick = (event) => {
   //make all input values null or shit
   employeeNoEL.value = "";
   firstNameEL.value = "";
@@ -26,10 +27,20 @@ resetBtnEL.addEventListener("click", (event) => {
   station3EL.value = "";
   employeeMailEL.value = "";
   employeePhoneEL.value = "";
-});
-
+};
+const submitHandler = (event) => {
+  //check form validity
+  if (!employeeAddForm.checkValidity()) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("not valid!");
+  }
+  employeeAddForm.classList.add("was-validated");
+};
+//EVENT LISTENERS
+resetBtnEL.addEventListener("click", resetBtnClick);
 //test
 window.onload = () => {
-  console.log(bodyEL);
-  console.log(resetBtnEL);
+  //add submit listener to form
+  employeeAddForm.addEventListener("submit", submitHandler);
 };
