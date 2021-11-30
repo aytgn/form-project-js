69fa4d59fe60619020953d8463697d3538bac89d
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-30 21:02:35 +0300

    adding classes

--- add-user.form.js
+++ add-user.form.js
@@ -1,101 +1,121 @@
-/***************************************SELECT ELEMENTS************************************************/
-//->form
-const employeeAddForm = document.querySelector("#employeeAddForm");
-const employeeNoEL = document.querySelector("#employeeNo");
-const firstNameEL = document.querySelector("#firstName");
-const employeeTitleEL = document.querySelector("#employeeTitle");
-const employeeRoleEL = document.querySelector("#employeeRole");
-const station1EL = document.querySelector("#station1");
-const station2EL = document.querySelector("#station2");
-const station3EL = document.querySelector("#station3");
-const employeeMailEL = document.querySelector("#employeeMail");
-const employeePhoneEL = document.querySelector("#employeePhone");
-const myRoleValidationEL = document.querySelector("#myRoleValidation");
-//->body
-const bodyEL = document.querySelector("body");
-//->buttons
-const resetBtnEL = document.querySelector("#resetFormBtn");
-const submitBtnEl = document.querySelector("#submitEmployeeForm");
 /***************************************CLASSES************************************************/
-
-/***************************************EVENT HANDLERS************************************************/
-const resetForm = () => {
-  //make all input values null or shit
-  employeeNoEL.value = "";
-  firstNameEL.value = "";
-  employeeTitleEL.value = "";
-  employeeRoleEL.value = "";
-  station1EL.value = "";
-  station2EL.value = "";
-  station3EL.value = "";
-  employeeMailEL.value = "";
-  employeePhoneEL.value = "";
-};
-const resetBtnClick = (event) => {
-  resetForm();
-};
-const submitHandler = (event) => {
-  //add validation class *MDB*
-  employeeAddForm.classList.add("was-validated");
-  //check if employee role selected manually
-  if (!employeeRoleEL.value) {
-    myRoleValidationEL.className = "myRoleInvalid";
-    event.preventDefault();
-    event.stopPropagation();
+class Employee {
+  constructor(no, name, title, role, sections, mail, phone) {
+    this.no = no;
+    this.name = name;
+    this.title = title;
+    this.role = role;
+    this.stations = sections;
+    this.mail = mail;
+    this.phone = phone;
   }
-  //check form validity *MDB*
-  else if (!employeeAddForm.checkValidity()) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
-  //if validated,
-  else {
-    event.preventDefault();
-    event.stopPropagation();
-    //INSTEAD OF SUBMIT, SAVE LOCALLY
-    // 1) Create an object with form-control-values
-    const employee = {
-      no: employeeNoEL.value,
-      firstName: firstNameEL.value,
-      title: employeeTitleEL.value,
-      role: employeeRoleEL.value,
-      stations: [station1EL.checked, station2EL.checked, station3EL.checked],
-      mail: employeeMailEL.value,
-      phone: employeePhoneEL.value,
-    };
-    // 2) Get local employeeArr
-    let employeeArrLS = localStorage.getItem("employeeArr");
-    //---->2.1)If no employeeArr, make it an empty array
-    if (!employeeArrLS) employeeArrLS = "[]";
-    //---->2.2)Convert to JSON
-    const employeeArrUI = JSON.parse(employeeArrLS);
-    // 3) Push employee to employeeArr
-    employeeArrUI.push(employee);
-    // 4) Stringify array and save to local storage
-    localStorage.setItem("employeeArr", JSON.stringify(employeeArrUI));
-    // 5) Clear input values
-    resetForm();
-    // 6) Show success Message!
-    //-----4.1) Add pre-defined class to myMessage div
-    document.querySelector("#myMessage").className =
-      "showMessageSuccess text-center";
-    //-----4.2) Add inner text to messageText element
-    document.querySelector("#messageText").innerText =
-      "Employee Saved Successfully!";
-    //-----4.3) Hide message with hiding element after 1.5sec
-    setTimeout(() => {
-      document.querySelector("#myMessage").className = "hideEl";
-    }, 1500);
-    // 7) Remove validation class(bcs we don't want to see any error after success until next submit)
-    employeeAddForm.classList.remove("was-validated");
-  }
-};
-const roleHandle = () => {
-  //whenever roleClicked, hide invalid message for role
-  myRoleValidationEL.className = "myRoleValid";
-};
-
+}
+class UI {
+  //VARIABLES
+  static employeeAddForm = document.querySelector("#employeeAddForm");
+  static employeeNoEL = document.querySelector("#employeeNo");
+  static firstNameEL = document.querySelector("#firstName");
+  static employeeTitleEL = document.querySelector("#employeeTitle");
+  static employeeRoleEL = document.querySelector("#employeeRole");
+  static station1EL = document.querySelector("#station1");
+  static station2EL = document.querySelector("#station2");
+  static station3EL = document.querySelector("#station3");
+  static employeeMailEL = document.querySelector("#employeeMail");
+  static employeePhoneEL = document.querySelector("#employeePhone");
+  static myRoleValidationEL = document.querySelector("#myRoleValidation");
+  static bodyEL = document.querySelector("body");
+  static resetBtnEL = document.querySelector("#resetFormBtn");
+  static submitBtnEl = document.querySelector("#submitEmployeeForm");
+  //METHODS
+  static resetForm = () => {
+    //make all input values null or shit
+    this.employeeNoEL.value = "";
+    this.firstNameEL.value = "";
+    this.employeeTitleEL.value = "";
+    this.employeeRoleEL.value = "";
+    this.station1EL.value = "";
+    this.station2EL.value = "";
+    this.station3EL.value = "";
+    this.employeeMailEL.value = "";
+    this.employeePhoneEL.value = "";
+  };
+  static resetBtnClick = () => {
+    this.resetForm();
+  };
+  static submitHandler = (event) => {
+    //add validation class *MDB*
+    this.employeeAddForm.classList.add("was-validated");
+    //check if employee role selected manually
+    if (!this.employeeRoleEL.value) {
+      this.myRoleValidationEL.className = "myRoleInvalid";
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    //check form validity *MDB*
+    else if (!this.employeeAddForm.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    //if validated,
+    else {
+      event.preventDefault();
+      event.stopPropagation();
+      //INSTEAD OF SUBMIT, SAVE LOCALLY
+      // 1) Create an object with form-control-values
+      const employee = new Employee(
+        UI.employeeNoEL.value,
+        UI.firstNameEL.value,
+        UI.employeeTitleEL.value,
+        UI.employeeRoleEL.value,
+        [UI.station1EL.checked, UI.station2EL.checked, UI.station3EL.checked],
+        UI.employeeMailEL.value,
+        UI.employeePhoneEL.value
+      );
+      // 2) Get local employeeArr
+      const employeeArrUI = LS.get("employeeArr");
+      // 3) Push employee to employeeArr
+      employeeArrUI.push(employee);
+      // 4) Stringify array and save to local storage
+      LS.set("employeeArr", employeeArrUI);
+      // 5) Clear input values
+      this.resetForm();
+      // 6) Show success Message!
+      //-----4.1) Add pre-defined class to myMessage div
+      document.querySelector("#myMessage").className =
+        "showMessageSuccess text-center";
+      //-----4.2) Add inner text to messageText element
+      document.querySelector("#messageText").innerText =
+        "Employee Saved Successfully!";
+      //-----4.3) Hide message with hiding element after 1.5sec
+      setTimeout(() => {
+        document.querySelector("#myMessage").className = "hideEl";
+      }, 1500);
+      // 7) Remove validation class(bcs we don't want to see any error after success until next submit)
+      employeeAddForm.classList.remove("was-validated");
+    }
+  };
+  static roleHandle = () => {
+    //whenever roleClicked, hide invalid message for role
+    this.myRoleValidationEL.className = "myRoleValid";
+  };
+}
+class LS {
+  static get = (key) => {
+    //if no ls,
+    if (!localStorage.getItem(key)) {
+      //return an empty array
+      return [];
+    } //else,
+    else {
+      //parse to json local storage item
+      return JSON.parse(localStorage.getItem(key));
+    }
+  };
+  static set = (key, value) => {
+    return localStorage.setItem(key, JSON.stringify(value));
+  };
+}
 /***************************************EVENT lISTENERS************************************************/
-resetBtnEL.addEventListener("click", resetBtnClick);
-employeeRoleEL.addEventListener("click", roleHandle);
-employeeAddForm.addEventListener("submit", submitHandler);
+UI.resetBtnEL.addEventListener("click", UI.resetBtnClick);
+UI.employeeRoleEL.addEventListener("click", UI.roleHandle);
+UI.employeeAddForm.addEventListener("submit", UI.submitHandler);
